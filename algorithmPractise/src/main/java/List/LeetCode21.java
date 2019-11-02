e0cd55405dcb067dd15e3a8872825326ad2e8099
庄旭 <shixi_zhuangxu@staff.weibo.com>
2019-11-02 23:28:37 +0800

    leetCode21 java

---
+++ algorithmPractise/src/main/java/List/LeetCode21.java
@@ -0,0 +1,20 @@
+package List;
+/**
+ * @program: LeetCodeAlgorithm
+ * @description: leetCode 21题  https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * @author: ZhuangXu
+ * @create: 2019-11-02 23:24
+ **/
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+
+public class LeetCode21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
