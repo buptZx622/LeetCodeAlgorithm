b7cce4c4cd56d5397da6582f60d97bbd4b4ee223
zhuangxu_sx <zhuangxu_sx@qiyi.com>
2019-11-04 17:07:44 +0800

    leetCode950 java

---
+++ algorithmPractise/src/main/java/Medium/Array/LeetCode950.java
@@ -0,0 +1,18 @@
+package main.java.Medium.Array;
+
+import java.util.Arrays;
+
+/**
+ * @author zhuangxu
+ * @date 2019/11/4 16:45
+ * leetcode 950 按递增顺序显示卡牌 https://leetcode-cn.com/problems/reveal-cards-in-increasing-order/
+ *  理解题意 将数组排序，进行一下逆操作
+ */
+public class LeetCode950 {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int[] res=new int[deck.length];
+        int index=0;
+        return res;
+    }
+}
