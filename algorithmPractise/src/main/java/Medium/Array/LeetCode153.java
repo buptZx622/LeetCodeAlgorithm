7d6119bf5a35621f46ac6b24220953806e756e6b
zhuangxu_sx <zhuangxu_sx@qiyi.com>
2019-11-13 20:17:28 +0800

    leetCode153

---
+++ algorithmPractise/src/main/java/Medium/Array/LeetCode153.java
@@ -0,0 +1,11 @@
+package main.java.Medium.Array;
+
+/**
+ * @author zhuangxu
+ * @date 2019/11/13 19:31
+ */
+public class LeetCode153 {
+    //题意本身很简单，但要求复杂度小于O(n)，可考虑二分法，关键点在于二分后左区间和右区间的选取条件
+    public int findMin(int[] nums) {
+    }
+}
